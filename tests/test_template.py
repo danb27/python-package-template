dd31d6413fa349569a50d5c4140286f94ecc5f4f
dan <danbianchini@outlook.com>
2024-04-06 06:41:03 -0700

    feat: add pre-commit test for template

--- tests/test_template.py
+++ tests/test_template.py
@@ -7,22 +7,38 @@ from cookiecutter.main import cookiecutter
 
 
 @pytest.fixture()
-def example_project_dir(tmp_path) -> Path:
-    template_path_os = cookiecutter(
+def cloned_directory(tmp_path):
+    original_dir = os.getcwd()
+    cloned_dir = cookiecutter(
         ".",
         no_input=True,
         output_dir=tmp_path,
     )
-    return Path(template_path_os)
+
+    os.chdir(cloned_dir)
+    try:
+        yield Path(cloned_dir)
+    except:  # noqa
+        os.chdir(original_dir)
+        raise
+    finally:
+        os.chdir(original_dir)
 
 
-def test_template(example_project_dir: Path):
-    env_path = example_project_dir / "env"
+@pytest.fixture()
+def example_project_env(cloned_directory):
+    env_path = Path(cloned_directory) / "env"
 
-    os.chdir(example_project_dir)
+    # create a dummy git repository (needed for pre-commit)
+    result = subprocess.run(["git", "init"], check=True, capture_output=True)
+    assert result.returncode == 0, f"Failed to create git repository"
+
+    # stage all files
+    result = subprocess.run(["git", "add", "."], check=True, capture_output=True)
+    assert result.returncode == 0, f"Failed to stage files"
 
     # create a new virtual environment
-    result = subprocess.run(["python3", "-m", "venv", str(env_path)], check=True, capture_output=True)
+    result = subprocess.run(["python3", "-m", "venv", str(env_path)], check=True)
     assert result.returncode == 0, f"Failed to create virtual environment"
 
     # activate the virtual environment and install dependencies
@@ -33,10 +49,26 @@ def test_template(example_project_dir: Path):
     ], check=True)
     assert result.returncode == 0, f"Failed to install dependencies"
 
+    yield env_path
+
+
+def test_template_pytest(example_project_env: Path):
     # run pytest in the virtual environment
     result = subprocess.run([
         "bash", "-c",
-        f"source {env_path / 'bin' / 'activate'} && "
+        f"source {example_project_env / 'bin' / 'activate'} && "
         "poetry run pytest"
     ], check=False)
     assert result.returncode == 0, f"Tests failed"
+
+
+def test_template_precommit(example_project_env: Path):
+    # run pre-commit in the virtual environment
+    result = subprocess.run([
+        "bash", "-c",
+        f"source {example_project_env / 'bin' / 'activate'} && "
+        "pre-commit install &&"
+        "pre-commit run --all-files && ls -la"
+    ], check=False)
+    print(os.listdir())
+    assert result.returncode == 0, f"pre-commit failed"
